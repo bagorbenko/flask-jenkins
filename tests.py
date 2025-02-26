1fa904de8851bd0f9a24634fed691a4e1501821f
Aliaksei <ba.gorbenko@gmail.com>
2025-02-27 00:04:03 +0100

    Updated message: hallo jenkins-test-1

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestApp(unittest.TestCase):
     def test_home(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "Hello, Jenkins!"})
+        self.assertEqual(response.json, {"message": "Hello, Man!"})
 
 if __name__ == '__main__':
     unittest.main()
