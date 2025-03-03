624fbda2603dd3b07243c6d3b2f9630a2a371185
Aliaksei <ba.gorbenko@gmail.com>
2025-03-03 17:11:32 +0100

    tesе2345

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, Guys!"})
+    return jsonify({"message": "Hello, TMS!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestApp(unittest.TestCase):
     def test_home(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "Hello, Guys!"})
+        self.assertEqual(response.json, {"message": "Hello, TMS!"})
 
 if __name__ == '__main__':
     unittest.main()
