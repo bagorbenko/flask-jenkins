c6d302eb6d0a916aedb81168ed72c9a5f3562718
Aliaksei <ba.gorbenko@gmail.com>
2025-03-24 19:33:21 +0100

    HW_4_black

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return jsonify({"message": "Hello, TMS!"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
--- tests.py
+++ tests.py
@@ -1,15 +1,17 @@
 import unittest
 from app import app
 
+
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
 
     def test_home(self):
-        response = self.app.get('/')
+        response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"message": "Hello, TMS!"})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
